942645056890406bcc655b4af43f7768e06d4764
kennyg37 <k.ganza@alustudent.com>
2024-08-17 10:59:07 +0200

    adding small changes

--- blog/imageUpload.py
+++ blog/imageUpload.py
@@ -5,6 +5,7 @@ class UploadImageToS3Mixin:
     def upload_image_to_s3(image, folder='images'):
         s3 = boto3.client(
             's3',
+            
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
             region_name=settings.AWS_S3_REGION_NAME
